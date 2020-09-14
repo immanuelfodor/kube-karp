eed9d0a212d509259a40fd619923bf37402e9c45
immanuelfodor <21174107+immanuelfodor@users.noreply.github.com>
2020-09-14 07:23:19 +0200

    Initial commit, add all project files

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM alpine
+
+RUN set -x \
+    && apk add --update --no-cache ucarp
+
+ENV KARP_INTERFACE eth0
+ENV KARP_HOST_IP=
+ENV KARP_VIRTUAL_IP 192.168.100.1
+ENV KARP_SUBNET 24
+ENV KARP_SERVER_ID 10
+ENV KARP_PASSWORD RAnDoM_max16char
+ENV KARP_UPSCRIPT /etc/ucarp/vip-up-default.sh
+ENV KARP_DOWNSCRIPT /etc/ucarp/vip-down-default.sh
+ENV KARP_EXTRA_FLAGS=
+ENV KARP_DEBUG=
+
+COPY docker-entrypoint.sh /
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# cleanup when the container is stopped or ucarp exits
+cleanup () {
+  kill -USR2 $(pidof ucarp)
+  sleep 1
+  $KARP_DOWNSCRIPT $KARP_INTERFACE $KARP_VIRTUAL_IP $KARP_SUBNET
+}
+trap "cleanup" SIGINT
+trap "cleanup" SIGTERM
+
+# set the debug env var to anything for debug logging
+if [ ! -z "${KARP_DEBUG}" ] ; then
+  set -x
+  cat /etc/conf.d/ucarp
+  cat /etc/init.d/ucarp
+  cat /etc/ucarp/vip-down-default.sh
+  cat /etc/ucarp/vip-up-default.sh
+  /usr/sbin/ucarp --help
+fi
+
+# if no explicit host ip is set, we determine it from the interface
+if [ -z "${KARP_HOST_IP}" ] ; then
+  KARP_HOST_IP=$(ip addr show ${KARP_INTERFACE} | grep inet | grep -v secondary | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}" | head -n 1)
+  echo "Auto host IP: ${KARP_HOST_IP}"
+fi
+
+# start up the service and put it into background
+/usr/sbin/ucarp \
+  --interface=${KARP_INTERFACE} \
+  --srcip=${KARP_HOST_IP} \
+  --vhid=${KARP_SERVER_ID} \
+  --pass=${KARP_PASSWORD} \
+  --addr=${KARP_VIRTUAL_IP} \
+  --upscript=${KARP_UPSCRIPT} \
+  --downscript=${KARP_DOWNSCRIPT} \
+  --xparam=${KARP_SUBNET} \
+  ${KARP_EXTRA_FLAGS} &
+
+# wait for the last process sent to background to finish (ucarp)
+wait $!
+
+# cleanup even if the process exits by itself
+# otherwise, the traps handle container stops initiated by the user
+cleanup
