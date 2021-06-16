7894a077eba6f2f8e5702d23a9b587bb5462a4eb
Tobias Florek <me@ibotty.net>
2021-06-16 16:02:21 +0200

    add healthcheck and release ip if not successful

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,9 @@
 
 # cleanup when the container is stopped or ucarp exits
 cleanup () {
-  kill -USR2 $(pidof ucarp)
+  kill -USR2 "$(pidof ucarp)"
   sleep 1
-  $KARP_DOWNSCRIPT $KARP_INTERFACE $KARP_VIRTUAL_IP $KARP_SUBNET
+  $KARP_DOWNSCRIPT "$KARP_INTERFACE" "$KARP_VIRTUAL_IP" "$KARP_SUBNET"
 }
 trap "cleanup" SIGINT
 trap "cleanup" SIGTERM
@@ -27,6 +27,7 @@ fi
 
 # start up the service and put it into background
 /usr/sbin/ucarp \
+  --daemonize \
   --interface=${KARP_INTERFACE} \
   --srcip=${KARP_HOST_IP} \
   --vhid=${KARP_SERVER_ID} \
@@ -35,10 +36,27 @@ fi
   --upscript=${KARP_UPSCRIPT} \
   --downscript=${KARP_DOWNSCRIPT} \
   --xparam=${KARP_SUBNET} \
-  ${KARP_EXTRA_FLAGS} &
+  ${KARP_EXTRA_FLAGS}
 
-# wait for the last process sent to background to finish (ucarp)
-wait $!
+demote_ucarp() {
+  kill -USR2 "$(pidof ucarp)"
+  sleep 1
+  $KARP_DOWNSCRIPT "$KARP_INTERFACE" "$KARP_VIRTUAL_IP" "$KARP_SUBNET"
+}
+
+do_healthcheck() {
+  [ "$(wget --no-check-certificate -q -O - "$KARP_HEALTHCHECK_URL")" = ok ]
+}
+
+# periodically check
+if [ "$KARP_DISABLE_HEALTHCHECK" = yes ]; then
+  sleep infinity
+else
+  while true; do
+    do_healthcheck || demote_ucarp
+    sleep 1
+  done
+fi
 
 # cleanup even if the process exits by itself
 # otherwise, the traps handle container stops initiated by the user
